2ca17ad96f3571fc0620732bbd678a5259d2b9df
Martin Gutschelhofer <martin.gutschelhofer@gmx.at>
2015-01-11 18:09:38 +0100

    Some cleanup

--- sudoko/src/mart/sudoko/Sudoko.java
+++ sudoko/src/mart/sudoko/Sudoko.java
@@ -2,15 +2,20 @@ package mart.sudoko;
 
 public class Sudoko {
 	
-	// The Sudoko holding all values
+	// The Sudoko matrix holding all values
 	private int fVariants[];
 	
-	// The array which defines the ordering what cells to resolve in what order
+	// The array which defines the ordering
+	// What cells to solve in which order
 	private int fPathOrder[];
 
-	private int fMaxLevel;
+	// Maximum value of a cell (typically 9)
 	private int fMaxValue;
+	
+	// Number of cells to resolve
 	private int fMaxPath;
+	
+	// The size of our Sudoko
 	private int fRows;
 	private int fCols;
 	
@@ -28,14 +33,13 @@ public class Sudoko {
 	public Sudoko() {
 		fRows = 9;
 		fCols = 9;
+		fSuds = 9; 
 
 		fMaxValue = 9;
 		fMaxPath  = 0;
-		fMaxLevel = fRows * fCols;
-		fSuds = 9; 
 
-		fVariants     = new int[fMaxLevel];
-		fPathOrder    = new int[fMaxLevel];
+		fVariants     = new int[fRows * fCols];
+		fPathOrder    = new int[fRows * fCols];
 		fRowValueUsed = new boolean[fRows][fMaxValue+1];
 		fColValueUsed = new boolean[fCols][fMaxValue+1];
 		fSudValueUsed = new boolean[fSuds][fMaxValue+1];
@@ -44,7 +48,158 @@ public class Sudoko {
 		fSetVals = 0;
 	}
 	
-	public int[] getMatrix()
+	public void init(int matrix[])
+	{
+		// First init the Sudo with the Game values
+		// int matrix[] = getMatrix();
+		for (int i=0; i< matrix.length; i++)
+		{
+			int value = matrix[i];
+			setValue(i, value);
+			
+			// Calculate a default path to solve that thing
+			// This is really the most stupid one
+			if(value==0) {
+				// No value was set; Need to calculate it!#
+				fPathOrder[fMaxPath++] = i;
+			}
+		}
+	}
+
+	// Print current processed variant
+	public void prettyPrintVariant()
+	{
+		for(int i=0; i<fRows * fCols; i++)
+		{
+			if (i>0 && i%fCols==0)
+				System.out.println();			
+			System.out.print(fVariants[i]);
+		}
+		System.out.println();
+		System.out.println();
+	}
+
+	// Check whether this value is ==allowed
+	boolean checkValue(int level, int value)
+	{
+		++fChkVals;
+		int row = getRow(level);
+		int col = getCol(level);
+		int sud = getSud(level);
+
+		// If one, or both values are true, they are used!
+		boolean used = fRowValueUsed[row][value] || 
+					fColValueUsed[col][value] || 
+					fSudValueUsed[sud][value];
+		return !used;
+	}	
+	
+	void setValue(int level, int value)
+	{
+		++fSetVals;
+		int row = getRow(level);
+		int col = getCol(level);
+		int sud = getSud(level);
+
+		// Reset usage of old value
+		int old = fVariants[level];
+		fRowValueUsed[row][old] = false;
+		fColValueUsed[col][old] = false;
+		fSudValueUsed[sud][old] = false;
+
+		// Mark these values as used
+		fRowValueUsed[row][value] = true;
+		fColValueUsed[col][value] = true;
+		fSudValueUsed[sud][value] = true;
+		fVariants[level] = value;	
+	}
+	
+	// For a given cell index return the row value
+	int getRow(int cellidx)
+	{
+		int row = cellidx / fCols;
+		return row;
+	}
+	
+	// For a given cell index return the column value
+	int getCol(int cellidx)
+	{
+		int col = cellidx % fRows;
+		return col;
+	}
+	
+	// For a given cell index return the Sudoko identifier/number
+	int getSud(int cellidx)
+	{
+		int row, col, sud;
+		row = getRow(cellidx);
+		col = getCol(cellidx);
+		
+		// ...I know only correct for certain constraints:
+		int j = (int)Math.sqrt(fSuds);
+		
+		sud = col / j + row / j * j;
+		return sud;
+	}
+
+	// Iterate over all variants
+	boolean iterateVariants()
+	{
+		boolean done=false;
+		int level=0;
+		boolean found;
+		
+		// Iterate until all variants have been processed
+		while(!done)
+		{
+			// Iterate to find the next allowed value
+			found = false;
+			int cellidx = fPathOrder[level];
+			for (int value=fVariants[cellidx]+1; value<=fMaxValue && !found; value++)
+			{
+				// Need to check whether this value is allowed
+				if (checkValue(cellidx, value))
+				{
+					// Great, we found a valid value on this level
+					found = true;
+					setValue(cellidx, value);
+					
+					// Debug
+					prettyPrintVariant();
+					
+					// We need to process the next level, or are we done?
+					++level;
+					if(level==fMaxPath)  {
+						// Yeah we are done!
+						done = true;
+					}
+				}
+			}
+			
+			if (!found) {
+				// Reached the max value on this level
+				// Must go one step up and proceed from there
+				setValue(cellidx, 0);
+				--level;
+				if(level<0) {
+					// OK, there is no solution!
+					done = true;
+				}
+			}	
+		}
+		
+		if (done) {
+			System.out.println("!!!SUCCESS!!!");
+			System.out.println("   Checked Values = " + fChkVals);
+			System.out.println("   Set Values     = " + fSetVals);
+		}
+		return done;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
 	{
 		int matrix0[] = new int[] {
 				
@@ -121,173 +276,9 @@ public class Sudoko {
 				8, 0, 7,   0, 0, 5,   2, 0, 0
 		};
 
-		return matrix4;
-	}
-	
-	public void init()
-	{
-		// First init the Sudo with the Game values
-		int matrix[] = getMatrix();
-		for (int i=0; i< matrix.length; i++)
-		{
-			int value = matrix[i];
-			setValue(i, value);
-			
-			// Calculate a default path to solve that thing
-			if(value==0) {
-				// No value was set; Need to calculate it!#
-				fPathOrder[fMaxPath++] = i;
-			}
-		}
-	}
-
-	// Print current processed variant
-	public void prettyPrintVariant(boolean deep)
-	{
-		for(int i=0; i<fMaxLevel; i++)
-		{
-			if (i>0 && i%fCols==0)
-				System.out.println();			
-			System.out.print(fVariants[i]);
-		}
-		System.out.println();
-		
-		if (deep)
-		{
-			for(int i=0;i<fRows;i++){
-				System.out.print("Row[" + i + "]:");
-				for (int j=1;j<=fMaxValue; j++){
-					if(fRowValueUsed[i][j])
-						System.out.print("T");
-					else
-						System.out.print("F");
-				}
-				System.out.println();
-			}
-		}
-		System.out.println();			
-	}
-
-	// Check whether this value is ==allowed
-	boolean checkValue(int level, int value)
-	{
-		++fChkVals;
-		int row = getRow(level);
-		int col = getCol(level);
-		int sud = getSud(level);
-
-		// If one, or both values are true, they are used!
-		boolean used = fRowValueUsed[row][value] || 
-					fColValueUsed[col][value] || 
-					fSudValueUsed[sud][value];
-		return !used;
-	}	
-	
-	void setValue(int level, int value)
-	{
-		++fSetVals;
-		int row = getRow(level);
-		int col = getCol(level);
-		int sud = getSud(level);
-
-		// Reset usage of old value
-		int old = fVariants[level];
-		fRowValueUsed[row][old] = false;
-		fColValueUsed[col][old] = false;
-		fSudValueUsed[sud][old] = false;
-
-		// Mark these values as used
-		fRowValueUsed[row][value] = true;
-		fColValueUsed[col][value] = true;
-		fSudValueUsed[sud][value] = true;
-		fVariants[level] = value;	
-	}
-	
-	int getRow(int level)
-	{
-		int row = level / fCols;
-		return row;
-	}
-	
-	int getCol(int level)
-	{
-		int col = level % fRows;
-		return col;
-	}
-	
-	int getSud(int level)
-	{
-		int row, col, sud;
-		row = getRow(level);
-		col = getCol(level);
-		
-		int j = (int)Math.sqrt(fSuds);
-		
-		sud = col / j + row / j * j;
-		return sud;
-	}
-
-	// Iterate over all variants
-	void iterateVariants()
-	{
-		boolean done=false;
-		int level=0;
-		boolean found;
-		
-		// Iterate until all variants have been processed
-		while(!done)
-		{
-			// Iterate to find the next allowed value
-			found = false;
-			int cellidx = fPathOrder[level];
-			for (int value=fVariants[cellidx]+1; value<=fMaxValue && !found; value++)
-			{
-				// Need to check whether this value is allowed
-				if (checkValue(cellidx, value))
-				{
-					// Great, we found a valid value on this level
-					found = true;
-					setValue(cellidx, value);
-					
-					// Debug
-					prettyPrintVariant(false);
-					
-					// We need to process the next level, or are we done?
-					++level;
-					if(level==fMaxPath)  {
-						// Yeah we are done!
-						System.out.println("!!!SUCCESS!!!");
-						System.out.println("   Checked Values = " + fChkVals);
-						System.out.println("   Set Values     = " + fSetVals);
-						
-						done = true;
-					}
-				}
-			}
-			
-			if (!found) {
-				// Reached the max value on this level
-				// Must go one step up and proceed from there
-				setValue(cellidx, 0);
-				--level;
-				if(level<0) {
-					// OK, there is no solution!
-					done = true;
-				}
-			}	
-		}
-	}
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello Sudoko");
-		
 		Sudoko sudoko = new Sudoko();
-		sudoko.init();
-		sudoko.prettyPrintVariant(false);
+		sudoko.init(matrix4);
+		sudoko.prettyPrintVariant();
 		sudoko.iterateVariants();
 	}
 }
